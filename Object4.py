6a9189a796b57c62dd390d5c9cdb7f32fd89896f
Nicko Buenviaje <74237653+FalsePatronus@users.noreply.github.com>
2022-07-14 12:08:30 +0800

    Create Object4.py

---
+++ Object4.py
@@ -0,0 +1,34 @@
+class Chair:
+    def __init__(self, c, m, co, b, h):
+        self.cushion = c
+        self.material = m
+        self.color = co
+        self.backrest = b
+        self.height = h
+    
+    def adjustHeight (self, h):
+        self.height = h
+    
+    def removeCushion(self):
+        self.cushion = ("Cushion Removed")
+
+    def addCushion(self):
+        self.cushion = ("You added another Cushion.")
+
+   #Never forget colons! All def must end with colon!
+    def recolor(self, co): 
+        self.color = co
+
+
+chair1 = Chair("Cushion Present", "Made of Wood", "Blue", "Backrest Absent", "1")
+
+print ("Is there a cushion? Answer: " + chair1.cushion)
+
+print ("The original color of the chair is " + chair1.color)
+chair1.recolor("pink")
+print ("The new color is " + chair1.color)
+
+print ("The original height is " + chair1.height + " meters")
+chair1.adjustHeight("2")
+print ("You added another " + chair1.height + " inches to the chair's height.")
+
