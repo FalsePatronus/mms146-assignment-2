c9a4077c62055d980a388897dceadc09783a145f
Nicko Buenviaje <74237653+FalsePatronus@users.noreply.github.com>
2022-07-15 11:53:33 +0800

    Update Object4.py

--- Object4.py
+++ Object4.py
@@ -19,10 +19,16 @@ class Chair:
     def recolor(self, co): 
         self.color = co
 
-
+#Assigning the values
 chair1 = Chair("Cushion Present", "Made of Wood", "Blue", "Backrest Absent", "1")
 
+
+#Program proper
 print ("Is there a cushion? Answer: " + chair1.cushion)
+chair1.addCushion()
+print (chair1.cushion)
+chair1.removeCushion()
+print (chair1.cushion)
 
 print ("The original color of the chair is " + chair1.color)
 chair1.recolor("pink")
@@ -31,4 +37,3 @@ print ("The new color is " + chair1.color)
 print ("The original height is " + chair1.height + " meters")
 chair1.adjustHeight("2")
 print ("You added another " + chair1.height + " inches to the chair's height.")
-
